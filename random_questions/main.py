3b55d692eaebcd273b8d73b2d338386f0ce8c392
Gautam Maurya <gautammaurya1299@gmail.com>
2025-07-01 13:04:24 +0530

    Add initial implementation of various Python examples and exercises

---
+++ random_questions/main.py
@@ -0,0 +1,65 @@
+
+# a=[1,2,3,5,3]
+# a.append(3)
+# print(a)
+
+# a.extend([4,5,6])
+# print(a)
+
+
+# =======================
+
+# dictionary = {
+#     "name": "John",
+#     "age": 30,
+#     "city": "New York"
+# }
+# for key, value in dictionary.items():
+#     print(key, value)
+
+
+# =====================================================
+
+# s = "The cat sat on the mat of cat mattress"
+
+# print(s)
+
+# a = s.replace("cat", "dog")
+# print(a)
+# print(s)
+
+# # output:  
+#         # The cat sat on the mat of cat mattress
+#         # The dog sat on the mat of dog mattress
+#         # The cat sat on the mat of cat mattress
+
+
+# ================================================
+
+# def factorial(n): return n * factorial(n-1) if n > 1 else 1
+
+# print(factorial(5))
+
+
+# ==============================================
+
+# strings = ["Python", "is", "awesome"] 
+# str=" ".join(strings)
+# print(str)  # Output: Python is awesome
+# print(strings)  # Output: ['Python', 'is', 'awesome']
+
+# =================================================
+
+# try:
+#     num = int(input("Enter a number: "))
+# except ValueError:
+#     print("That's not a valid number!")
+
+
+# =================================================
+
+
+# result = int("10.5") + 5; print(result)
+# result = float("10.5") + 5; print(result)
+# result = str(10.5 + 5); print(result)
+# result = "10.5" + str(5); print(result)
