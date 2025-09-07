4bd97ae27ab53fac3e7c4223f2523e39bf271b70
Gautam Maurya <gautammaurya1299@gmail.com>
2025-09-08 00:00:20 +0530

    Remove unused audio files and deprecated text-to-speech modules; implement new gTTS functionality in gtt_text_to_speech.py

--- text-to-voice/speech_text.py
+++ text-to-voice/speech_text.py
@@ -1,30 +1,43 @@
 import pyttsx3
-import random
-# engine = pyttsx3.init()
-# engine.say("Hello, this is a test.")
-# engine.runAndWait()
-def tell(txt:str,speed=None):
+import time
+import google.generativeai as genai
+
+# Configure your API key
+# Replace "YOUR_API_KEY" with your actual API key
+genai.configure(api_key="AIzaSyCTcndwqmw65Xonykm9twax1VlsbpFNAd8") 
+
+# For a synchronous call (most common for simple requests)
+model = genai.GenerativeModel('gemini-2.5-flash') # Using 'gemini-pro' as 'gemini-2.5-flash' might be a future or internal model name
+response = model.generate_content("discribe in 5 lines,use professional words:-" + "tell some interesting about google")
+txt= response.text
+print(txt)
+
+
+# # If you were working with streaming responses (for longer outputs)
+# response_stream = model.generate_content("discribe in 5 lines,use professional words"+"tell some interesting about google", stream=True)
+# for chunk in response_stream:
+#     print(chunk.text)
+#     time.sleep(1)
+
+
+
+
+def tell(txt:str,speed=100):
     engine = pyttsx3.init()
     if speed:
-        engine.setProperty("rate",speed)
-        word_rate=engine.getProperty("rate")
-        # word_rate=speed
-        print("word_rate: ",word_rate)
-    engine.say(txt,)
+        engine.setProperty('rate', value=speed)
+    engine.say(txt)
     engine.runAndWait()
-    
+    engine.stop()
 
-# pyttsx3.getProperty("")
-data="this is the test i know but you need to utilize all the resources for get a best result"
-for i in data.split(" "):
-    
-    tell(i,speed=random.randint(50,200))
-    # engine = pyttsx3.init()
-    # engine.say(i)
-    # engine.runAndWait()
-    
-    
-    
-    
-    
-    # pyttsx3.speak(i)
+def speech(txt:str):
+    tell("starting the speech")
+    # tell(txt)
+    for i in txt.split(" "):
+        n=len(i)
+        slp=max(0,n*0.01)
+        print(i," ")
+        tell(i)
+        time.sleep(slp)
+
+# speech(txt)
