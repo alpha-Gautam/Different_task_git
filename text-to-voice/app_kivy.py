6e02a3178820540291ad2739aa3b17ea58c6f74a
Gautam Maurya <gautammaurya1299@gmail.com>
2025-09-24 21:55:22 +0530

    Update .gitignore and enhance Kivy app layout with a clear button functionality

--- text-to-voice/app_kivy.py
+++ text-to-voice/app_kivy.py
@@ -11,12 +11,15 @@ class MyApp(App):
 
         self.text_input = TextInput(hint_text="Enter your name")
         self.button = Button(text="Say Hello")
+        self.button2 = Button(text="Clear")
         self.label = Label(text="")
 
         self.button.bind(on_press=self.say_hello)
+        self.button2.bind(on_press=self.clear_text)
 
         self.layout.add_widget(self.text_input)
         self.layout.add_widget(self.button)
+        self.layout.add_widget(self.button2)
         self.layout.add_widget(self.label)
 
         return self.layout
@@ -24,6 +27,10 @@ class MyApp(App):
     def say_hello(self, instance):
         name = self.text_input.text
         self.label.text = f"Hello, {name}!"
+
+    def clear_text(self, instance):
+        self.text_input.text = ""
+        self.label.text = ""
 
 
 if __name__ == "__main__":
